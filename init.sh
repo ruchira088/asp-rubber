79403c5c19177519a93d815917e1aa2b27bb6d43
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2018-01-10 20:34:20 +1100

    Adding init.sh implementation

--- init.sh
+++ init.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+
+GIT_REPOS=(
+    "git@github.com:ruchira088/api-gateway.git"
+    "git@github.com:ruchira088/product-service.git"
+    "git@github.com:ruchira088/asp-rubber-web-app.git"
+)
+
+for gitRepo in "${GIT_REPOS[@]}"
+do
+    git clone ${gitRepo}
+done
